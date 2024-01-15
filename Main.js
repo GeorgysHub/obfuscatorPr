644b81073f9f389f2840e53d544284ef46ebf378
Gosha <gosha.balashov.1000@gmail.com>
2024-01-15 23:36:50 +0400

    commit13

--- Main.js
+++ Main.js
@@ -1,8 +1,21 @@
-function execute() {
+/*function execute() {
     require('./src/AddVariables.js');
     require('./src/DeleteComments.js');
     require('./src/Name.js');
     require('./src/Semi.js');
     require('./src/DeleteSpaces.js');
 }
-  execute(); 
+  execute(); */
+  function execute() {
+    const operations = [
+        './src/AddVariables.js',
+        './src/DeleteComments.js',
+        './src/Name.js',
+        './src/Semi.js',
+        './src/DeleteSpaces.js'
+    ];
+
+    operations.forEach(operation => require(operation));
+}
+
+execute();
